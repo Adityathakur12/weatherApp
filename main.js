18b6314e2921828aca1ab1d35bdc393d5e0c533a
Adityathakur12 <114096753+Adityathakur12@users.noreply.github.com>
2023-01-08 12:29:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+const tempField = document.querySelector(".temp .temp_f")
+const p_lace = document.querySelector(".temp .place")
+const icon = document.querySelector(".img img")
+const searchField = document.querySelector(".search")
+const form = document.querySelector("form")
+
+
+let target="patna"
+const datafetch = async (target) => {
+  try{
+    
+  
+
+    const url =  `https://api.weatherapi.com/v1/current.json?key=429ec1e6014044f5a5a95806230701&q=${target}&aqi=yes`
+
+    const responce = await fetch(url);
+
+    const data = await responce.json();
+
+    
+    
+    const {
+      current:{temp_c,condition:{icon}},
+      location:{name}
+    } = data
+    const temp_deg = `${temp_c}°C`
+  updatedom(temp_deg,name,icon);
+}
+catch(error){
+  alert("location not found ");
+}
+}
+function updatedom(temprature,citi,emoji){
+  
+  tempField.innerText = temprature;
+  p_lace.innerText = citi;
+  icon.src = emoji;
+}
+
+datafetch(target);
+
+
+form.addEventListener("submit",search);
+
+function search(e) {
+  e.preventDefault();
+
+  target = searchField.value;
+
+  datafetch(target);
+}
+ 
+ 
